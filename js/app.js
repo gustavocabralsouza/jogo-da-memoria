fa3501da040a733d6e78fcfe88878422ff4c6a27
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2019-04-16 06:28:22 -0300

    update nas funções de exibição e verificação de resultados dos cards

--- js/app.js
+++ js/app.js
@@ -3,11 +3,41 @@ jQuery(function () {
 });
 
 function main() {
-  // const activeCards = '.container-game .card.show';
 
-  $('.card').on('click', function () {
+  $('.card').on('click', function (e) {
+    //clicked on card that contains the show class
+    if (e.currentTarget.className.indexOf('show') > -1) {
+      return;
+    }
+
+    const activeCards = '.card.show';
+    const activeCardsValues = '.card.show .card-back span';
+
+    //até dois cards ativos
+    if ($(activeCards).length === 2) {
+      hideCards($(activeCards));
+    }
+
+    //exibir valor do card
     showCard($(this));
+
+    //verificando valores
+    if ($(activeCards).length === 2) {
+      // hideCards($(activeCards));
+      checkCards(activeCardsValues, activeCards);
+    }
+
   });
 
+
   const showCard = (elem) => elem.toggleClass('show');
+  const hideCards = (elem) => elem.removeClass('show');
+
+  const checkCards = (elem, activeCards) => {
+    if ($(elem)[0].textContent === $(elem)[1].textContent) {
+      $($(activeCards)[0]).addClass('verified')
+    } else {
+      setTimeout(() => hideCards($(activeCards)), 1500);
+    }
+  }
 }
