5d87aac4f2f36d08e0b27c77336e8b656d6b8a94
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2019-04-16 07:02:06 -0300

    otimização das funções

--- js/app.js
+++ js/app.js
@@ -3,17 +3,16 @@ jQuery(function () {
 });
 
 function main() {
-
   $('.card').on('click', function (e) {
     //clicked on card that contains the show class
     if (e.currentTarget.className.indexOf('show') > -1) {
       return;
     }
 
-    const activeCards = '.card.show';
-    const activeCardsValues = '.card.show .card-back span';
+    const activeCards = '.card.show:not(.verified)';
+    const activeCardsValues = '.card.show:not(.verified) .card-back span';
 
-    //até dois cards ativos
+    // até dois cards ativos
     if ($(activeCards).length === 2) {
       hideCards($(activeCards));
     }
@@ -23,21 +22,22 @@ function main() {
 
     //verificando valores
     if ($(activeCards).length === 2) {
-      // hideCards($(activeCards));
       checkCards(activeCardsValues, activeCards);
     }
-
   });
 
-
   const showCard = (elem) => elem.toggleClass('show');
-  const hideCards = (elem) => elem.removeClass('show');
+  const hideCards = (elem) => {
+    if(elem.length > 1){
+      elem.removeClass('show');
+    }
+  }
 
-  const checkCards = (elem, activeCards) => {
-    if ($(elem)[0].textContent === $(elem)[1].textContent) {
-      $($(activeCards)[0]).addClass('verified')
+  const checkCards = (cards, activeCards) => {
+    if ($(cards)[0].textContent === $(cards)[1].textContent) {
+      $(activeCards).addClass('verified')
     } else {
-      setTimeout(() => hideCards($(activeCards)), 1500);
+      setTimeout(() => hideCards($(activeCards)), 1200);
     }
   }
 }
